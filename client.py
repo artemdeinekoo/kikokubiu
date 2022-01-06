be2f8420aca74e1ffe3d45b36efaa1d25154744c
Artem Deineko <artemdeinekoo@gmail.com>
2022-01-06 16:21:07 +0200

    cart added and bot works without errors

--- client.py
+++ client.py
@@ -1,9 +1,29 @@
+import sqlite3
+
+
 class Client:
     def __init__(self):
         self.cart = []
+        self.sum = 0
 
-    def showCart(self):
-        if len(self.cart) == 0:
-            return "Наразі ваша корзина пуста 😕"
-        else:
-            return self.cart
+    def showSubtotal(self):
+        return f"Разом: {self.sum} ₴"
+
+    def clearCart(self):
+        self.cart.clear()
+        self.sum = 0
+
+    def getCart(self):
+        for i in range(len(self.cart)):
+                a = self.cart[i]
+
+        return a
+
+    def acceptOrder(self, client_id, client_name, db_file = 'database.db'):
+        for i in range(len(self.cart)):
+            self.conn = sqlite3.connect(db_file, check_same_thread = False)
+            self.cursor = self.conn.cursor()
+            self.cursor.execute("""INSERT INTO orders(client, client_name, cart, sum) 
+            VALUES(?, ?, ?, ?);""", (client_id, client_name, self.cart[i], self.sum))
+        
+            self.conn.commit()
